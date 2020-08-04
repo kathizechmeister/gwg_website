269cd244aad0f82a7854a31b16489633f0fd9207
kathizechmeister <65546336+kathizechmeister@users.noreply.github.com>
2020-08-04 15:52:26 +0200

    Add files via upload

--- bootstrap-4.5.0-dist/bootstrap-4.5.0-dist/js/function.js
+++ bootstrap-4.5.0-dist/bootstrap-4.5.0-dist/js/function.js
@@ -1,3 +1,5 @@
+
+
 $('.carousel').carousel({
     interval: false,
 });
@@ -155,3 +157,42 @@ function toggleMaps() {
 
 
 }
+
+
+/*********//*
+document.querySelector('button.image').onclick = () => {
+
+    basicLightbox.create(`
+		<img width="1400" height="900" src="https://placehold.it/1400x900">
+	`).show()
+
+}*/
+
+
+
+
+/********************************************************************/
+const lightbox = document.createElement('div');
+lightbox.id = 'lightbox';
+document.body.appendChild(lightbox);
+
+const images = document.querySelectorAll('.lightbox ul li img');
+images.forEach(image => {
+    image.addEventListener('click', e => {
+        lightbox.classList.add('active');
+        const img = document.createElement('img');
+        img.src = image.src;
+        while (lightbox.firstChild) {
+            lightbox.removeChild(lightbox.firstChild)
+        }
+        lightbox.appendChild(img)
+    })
+});
+
+lightbox.addEventListener('click', e => {
+    if (e.target !== e.currentTarget) return;
+    lightbox.classList.remove('active')
+});
+
+/**************************************/
+
